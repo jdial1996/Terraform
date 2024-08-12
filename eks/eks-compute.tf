b6503f8326999dc0668862f64ff582121d718845
Jasdeep Singh <jsd@Jasdeeps-MacBook-Pro.local>
2024-08-12 23:32:03 +0100

    add condition to ebs csi driver policy attachment

--- eks/eks-compute.tf
+++ eks/eks-compute.tf
@@ -43,6 +43,7 @@ resource "aws_iam_role_policy_attachment" "eks-worker-node-logs-policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "eks-worker-node-ebs-csi-driver" {
+  count = var.enable_ebs_csi_controller ? 1 : 0 
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.nodegroup_role.name
 }
